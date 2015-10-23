50c5b00cda01571a6d8972bf8b5796fc3d4aac5b
Peter Clayder <peterclayder@gmail.com>
2015-10-23 21:27:49 -0200

    iniciando correção na classe adt

--- caminho.php
+++ caminho.php
@@ -3,21 +3,26 @@
 include 'classe/Primeiro_metodo.php';
 include 'classe/Add.php';
 
+/*
 if (isset($_POST['valida'])) {
     $matriz = $_POST['matriz'];    
     $qtdGrafos = $_POST['qtdGrafos'];
     
     /*
      * Insere o custo do caminho do grafo i até j,no custo do caminho do grafo j até i
-    */
+    
 
     for ($i = 0; $i < $qtdGrafos; $i++) {
         for ($j = 0; $j < $qtdGrafos; $j++) {
             $matriz[$j][$i] = $matriz[$i][$j];
         }
     }
+}
+else{
+    header('location: index.php');   
+}
+*/
 
-/*
     $qtdGrafos = 5;
 
     $matriz[0][0] = 0;
@@ -49,7 +54,7 @@ if (isset($_POST['valida'])) {
     $matriz[4][2] = 7;
     $matriz[4][3] = 3;
     $matriz[4][1] = 6;
-    */
+    
 
     echo "************ Matriz ************";
     echo "</br>";
@@ -58,64 +63,164 @@ if (isset($_POST['valida'])) {
     echo "</pre>";
     echo "</br>";
     echo "********************************";
-    
-    echo "</br>";
-    echo "************ Resultado Primeiro Método ************ ";
-    echo "</br>";
-    $metodo = new Primeiro_metodo($matriz, $qtdGrafos);
-    // $caminho recebe o array com os caminhos
-    $caminho = $metodo->caminho();
-    // $tam recebe o tamanho do array $caminho
-    $tam = count($caminho);
-    echo "</br>";
-    echo "Custo = " . $metodo->getCusto();
-    echo "</br>";
-    echo "Caminho = ";
-    for($i = 0; $i < $tam; $i++){
-        // Verifica se é o ultimo grafo
-        if($i != $tam -1){
-            echo $caminho[$i]."->";
-        }
-        else{
-            echo $caminho[$i];
-        }       
-    }
-    echo "</br>";
-    echo "********************************";
-    
-    
-    
-    
-    echo "</br>";
-    echo "************ Resultado Método ADD************ ";
-    echo "</br>";
-    $add = new Add($matriz, $qtdGrafos);
-    // $caminho recebe o array com os caminhos
-    $caminhoAdd = $add->getArrayCaminho();
-    // $tam recebe o tamanho do array $caminho
-    $tamAdd = count($caminhoAdd);
-    echo "</br>";
-    echo "Custo = " . $add->getCusto();
-    echo "</br>";
-    echo "Caminho = ";
-    for($i = 0; $i < $tamAdd; $i++){
-        // Verifica se é o ultimo grafo
-        if($i != $tamAdd -1){
-            echo $caminhoAdd[$i]."->";
-        }
-        else{
-            echo $caminhoAdd[$i];
-        }       
-    }
-    echo "</br>";
-    
-}
-else{
-    header('location: index.php');   
-}
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Problema do Caixeiro Viajante</title>
+    <link href="style/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style/css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <center><strong><h1 style="text-decoration: underline">Problema do caixeiro viajante</h1></strong></center>
+                <table class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th><center>Ciclo</center></th>
+                      <th><center>Custo</center></th>
+                    </tr>
+                  </thead>
+                  <tbody>      
+            <?php 
+                    for ($i = 0; $i < $qtdGrafos; $i++) {
+                        for ($j = $i; $j < $qtdGrafos; $j++) {
+                            if($i != $j){
+                                echo "<tr>";
+                                ?>
+                                    <th scope='row'>
+                                        <div class="circulo">
+                                            <div class="circle">
+                                               <p class='caminho'> <?php echo $i?><p>
+                                            </div>
+                                        </div>
+                                        <div class="divisor">
+                                            <span style="font-size: 30px" class="glyphicon glyphicon-arrow-right"></span>
+                                        </div>
+                                        <div class="circulo">
+                                            <div class="circle">
+                                               <p class='caminho'> <?php echo $j?><p>
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <?php
+                                    echo "<td>". $matriz[$i][$j]."</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    }
+            ?> 
+                  </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <center><h3>Método do Vizinho Mais Próximo</h3></center>
+                <?php $metodo = new Primeiro_metodo($matriz, $qtdGrafos); ?>
+                <?php // $caminho recebe o array com os caminhos ?>
+                <?php $caminho = $metodo->caminho(); ?>
+                <?php // $tam recebe o tamanho do array $caminho ?>
+                <?php $tam = count($caminho); ?>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                          <th><center>Caminho</center></th>
+                          <th><center>Custo</center></th>
+                        </tr>
+                    </thead>
+                        <tbody>
+                            <tr>
+                                <td>                              
+                                  <?php
+                                    for($i = 0; $i < $tam; $i++){
+                                        // Verifica se é o ultimo grafo
+                                        if($i != $tam -1){
+                                            ?>
+                                            <div class="circulo">
+                                                <div class="circle">
+                                                   <p class='caminho'> <?php echo $caminho[$i]?><p>
+                                                </div>
+                                            </div>
+                                            <div class="divisor">
+                                                <span style="font-size: 30px" class="glyphicon glyphicon-arrow-right"></span>
+                                            </div>
+                                            <?php
+                                        }
+                                        else{
+                                            ?>
+                                            <div class="circulo">
+                                                <div class="circle">
+                                                    <p class='caminho' ><?php echo $caminho[$i]; ?></p>
+                                                </div>
+                                            </div>
+                                            <?php 
+                                        }       
+                                    }
+                                  ?>
+                                </td>
+                                <td><center><?php echo $metodo->getCusto(); ?></center></td>
+                            </tr>
+                        </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <center><h3>Método ADT</h3></center>
+                <?php $add = new Add($matriz, $qtdGrafos); ?>
+                <?php // $caminho recebe o array com os caminhos ?>
+                <?php $caminhoAdd = $add->getArrayCaminho(); ?>
+                <?php  // $tam recebe o tamanho do array $caminho ?>
+                <?php $tamAdd = count($caminhoAdd); ?>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                          <th><center>Caminho</center></th>
+                          <th><center>Custo</center></th>
+                        </tr>
+                    </thead>
+                        <tbody>
+                            <tr>
+                                <td>                              
+                                  <?php
+                                    for($i = 0; $i < $tamAdd; $i++){
+                                        // Verifica se é o ultimo grafo
+                                         if($i != $tamAdd -1){
+                                            ?>
+                                            <div class="circulo">
+                                                <div class="circle">
+                                                   <p class='caminho'> <?php echo $caminhoAdd[$i]?><p>
+                                                </div>
+                                            </div>
+                                            <div class="divisor">
+                                                <span style="font-size: 30px" class="glyphicon glyphicon-arrow-right"></span>
+                                            </div>
+                                            <?php
+                                        }
+                                        else{
+                                            ?>
+                                            <div class="circulo">
+                                                <div class="circle">
+                                                    <p class='caminho' ><?php echo $caminhoAdd[$i]; ?></p>
+                                                </div>
+                                            </div>
+                                            <?php 
+                                        }       
+                                    }
+                                  ?>
+                                </td>
+                                <td><center><?php echo $add->getCusto() ?></center></td>
+                            </tr>
+                        </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
   
-
-
-
-
-
--- index.php
+++ index.php
@@ -5,50 +5,95 @@ if (isset($_POST['valida'])) {
     $qtdGrafo = $_POST['qtdGrafo'];
 }
 
-if ($qtdGrafo != NULL) {
-    echo "</br>";
-    $custoIgual = 0;
-    echo "<form method='POST' action='caminho.php'>";
-        echo "<input type='hidden' value='1' name='valida'>";
-        echo "<input type='hidden' value='$qtdGrafo' name='qtdGrafos'>";
-        for ($i = 0; $i < $qtdGrafo; $i++) {
-            for ($j = $i; $j < $qtdGrafo; $j++) {
-                if($i == $j){
-                    echo "<input type='hidden' value='0' name='matriz[".$i."][".$j."]'>";
-                }
-                else if($j < $custoIgual){
-                    echo "<input type='hidden' value='I' name='matriz[".$i."][".$j."]'>";
-                }
-                    
-                else{
-                    echo "</br>";
-                    echo "Forneca o custo do grafo  " . $i . " para o grafo  " . $j. "  ";
-                    echo "<input type='text' name='matriz[".$i."][".$j."]' placeholder='Custo ?'>";
-                    echo "</br>";
-                }
-                
-            }
-        }  
-        echo "</br>";
-        echo "<input type='submit' name='inserir' value='Gerar Matriz'>";
-    echo "</form>";
-}
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Problema do Caixeiro Viajante</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="style/css/bootstrap.min.css" rel="stylesheet">
+        <link href="style/css/style.css" rel="stylesheet">
     </head>
     <body>
-        <?php if($qtdGrafo == NULL): ?>
+    <center>
+        <strong><h1 style="text-decoration: underline">Problema do caixeiro viajante</h1></strong>
+        <?php if($qtdGrafo == NULL){ ?>
             <form method="POST" action="index.php">
                 <input type="hidden" value="1" name="valida">
-                <input type="text" name="qtdGrafo" placeholder="Quantos grafos ?">
-                <input type="submit" name="inserir" value="Inserir">
+                <input type="number" name="qtdGrafo" placeholder="Quantos grafos ?">
+                <input class="btn btn-success" type="submit" name="inserir" value="Inserir">
             </form>
-        <?php endif;?>
+        <?php }
+        else {
+            echo "</br>";
+            $custoIgual = 0;
+            echo "<form method='POST' action='caminho.php'>";
+                echo "<input type='hidden' value='1' name='valida'>";
+                echo "<input type='hidden' value='$qtdGrafo' name='qtdGrafos'>";
+?>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                  <th>Ciclo</th>
+                                  <th>Custo</th>
+                                </tr>
+                            </thead>
+                            <tbody>                           
+<?php  
+                                for ($i = 0; $i < $qtdGrafo; $i++) {
+                                    for ($j = $i; $j < $qtdGrafo; $j++) {
+                                        
+                                        if($i == $j){
+                                            echo "<input type='hidden' value='0' name='matriz[".$i."][".$j."]'>";
+                                        }
+                                        else if($j < $custoIgual){
+                                            echo "<input type='hidden' value='I' name='matriz[".$i."][".$j."]'>";
+                                        }
+                                            
+                                        else{
+                                            echo "<tr>";
+                                            ?>
+                                            <td>
+                                                <div class="circulo">
+                                                    <div class="circle">
+                                                       <p class='caminho'> <?php echo $i;?><p>
+                                                    </div>
+                                                </div>
+                                                <div class="divisor">
+                                                    <span style="font-size: 30px" class="glyphicon glyphicon-arrow-right"></span>
+                                                </div>
+                                                <div class="circulo">
+                                                    <div class="circle">
+                                                       <p class='caminho'> <?php echo $j;?><p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <?php
+                                              echo "<td>  <input type='number' name='matriz[".$i."][".$j."]'> </td>";
+                                            echo "</tr>";
+                                        }
+                                    }
+                                } 
+                                ?> 
+                            </tbody>
+                        </table>
+                      </div>
+                    </div>
+                </div>
+                <?php
+                echo "</br>";
+                echo "<input class='btn btn-success btn-lg btn-block' type='submit' name='inserir' value='Gerar Caminho'>";
+            echo "</form>";
+        }
+            ?>
+        </center>
     </body>
 </html>
+
 
